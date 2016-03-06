70f58c7900c95cb41e55658b8eb0ead3fb5f5d85
Jon de la Motte <jdelamotte@appnexus.com>
2016-03-05 19:37:00 -0800

    Mert

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var ReactDOMServer = require('react-dom/server');
 var fs = require('fs-extra');
 var less = require('less');
 var webpack = require('webpack');
+var contentful = require('contentful');
 
 var DIST_DIR = `${__dirname}/dist`;
 
@@ -12,6 +13,15 @@ var rawHtml = fs.readFileSync('./src/templates/index.html', 'utf8');
 var rawLess = fs.readFileSync('./src/styles/index.less', 'utf8');
 
 var pages = requireAll({ dirname: __dirname + '/src/pages' });
+
+var client = contentful.createClient({
+  space: 'lcngqe23n7gl',
+  accessToken: process.env.ACCESS_TOKEN
+});
+
+client.getContentType('2wKn6yEnZewu2SCCkus4as') // Post
+  .then(posts => console.log(posts.items))
+  .catch(e => console.error(e))
 
 less.render(rawLess, { compress: true }).then((output) => {
   _.forEach(pages, (pageDefinition, pageName) => {
